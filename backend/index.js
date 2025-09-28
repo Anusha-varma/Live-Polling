c6b22ba703f242c77d7a1ac3858b45ae4324b2a6
Anusha-varma <152601876+Anusha-varma@users.noreply.github.com>
2025-09-29 01:40:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,12 @@ app.use(cors());
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/pollApp")
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ MongoDB Error:", err));
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // Schema
 const questionSchema = new mongoose.Schema({
